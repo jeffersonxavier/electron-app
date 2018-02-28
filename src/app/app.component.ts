7a75bedfb8493ddf2e55997126b141d5a65e4b76
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2018-02-28 18:01:15 -0300

    adding ngx-electron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private electronService: ElectronService) {}
+
+  launchWindow() {
+    console.log('launchWindow....');
+    this.electronService.shell.openExternal('https://github.com/ThorstenHans/ngx-electron');
+  }
 }
