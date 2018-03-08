c6264e63dbf221e4806d30ae969e6c37630a74c7
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2018-03-08 14:50:12 -0300

    initing musics colections feature

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,6 +9,8 @@ import { HymnalService, Hymnal, MusicLetter, Strophe } from '../../modules/hymna
 })
 export class HomeComponent {
 
+  showMusicOptions: Boolean = false;
+
   constructor(private electronManagerService: ElectronManagerService, private hymnalService: HymnalService) {}
 
   openSwiperExample() {
@@ -20,44 +22,5 @@ export class HomeComponent {
     this.hymnalService.createHymnal(hymnal)
       .then((hymnal: Hymnal) => console.log(hymnal))
       .catch((err) => console.log(err));
-
-    // let music: MusicLetter = new MusicLetter();
-    // music.title = 'Chuvas de Graça';
-    // music.author = 'J.R. (J.McGranahan)';
-    // music.musicNumber = 1;
-    // music.chorus = new Strophe(0, [
-    //   'Chuvas de graça,',
-    //   'Chuvas pedimos, Senhor;',
-    //   'Manda-nos chuvas constantes,',
-    //   'Chuvas do Consolador.',
-    // ]);
-    // music.strophes = [
-    //   new Strophe(1, [
-    //     'Deus prometeu com certeza',
-    //     'Chuvas de graça mandar;',
-    //     'Ele nos dá fortaleza,',
-    //     'E ricas bênçãos sem par',
-    //   ]),
-    //   new Strophe(2, [
-    //     'Cristo nos tem concedido',
-    //     'O santo Consolador,',
-    //     'De plena paz nos enchido,',
-    //     'Para o reinado do amor.',
-    //   ]),
-    //   new Strophe(3, [
-    //     'Dá-nos, Senhor, amplamente,',
-    //     'Teu grande gozo e poder;',
-    //     'Fonte de amor permanente,',
-    //     'Põe dentro de nosso ser.',
-    //   ]),
-    //   new Strophe(4, [
-    //     'Faze os teus servos piedosos,',
-    //     'Dá-lhes virtude e valor,',
-    //     'Dando os teus dons preciosos,',
-    //     'Do santo Preceptor.',
-    //   ]),
-    // ];
-
-    // console.log(JSON.stringify(music));
   }
 }
